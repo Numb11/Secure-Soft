f3a85238b36a546b6023fed6faa52b79c35edbd1
Soup <87945079+Numb11@users.noreply.github.com>
2025-02-17 14:00:40 +0000

    Added stuff

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     
+<<<<<<< Updated upstream
 
 
 <head>
@@ -38,6 +39,42 @@
     <?php
         
     ?>
+=======
+
+
+<head>
+<title> FakeBook </title>
+<link rel = "stylesheet" href="log-in.css">
+<link rel="icon" href="favi.png">
+</head>
+
+<body>
+    <h1 id = "bannerText">Connection made easier </h1>
+    <div class = "logInForm">
+        <form action="verifyUser.php" method = "POST">
+            <label for="username">Username:</label><br>
+            <input type="email" placeholder = "Enter Email" name="username"><br>
+
+            <label for="password">Password: :</label><br>
+            <input type="password" placeholder = "Enter Password"  name="password">
+        </form>
+
+        <br>
+
+        <button type = "submit">Login</button>
+
+        <label>
+                <input type="checkbox" checked="checked" name="remember">
+                Remember me  
+        </label>
+
+    
+
+        <br></br>
+
+        <label> <a href="signup.html">Sign-up here</a> </label>
+    </div>
+>>>>>>> Stashed changes
 
 </body>
 </html>
---
+++ verifyUser.php
@@ -0,0 +1,21 @@
+<?php
+    $email = $_POST['username'];
+    $password = $_POST['password'];
+
+    echo $email
+    echo $password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>
