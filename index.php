df5df67117f621e1fb9318cd6a71e5e8b99ab1b1
Soup <87945079+Numb11@users.noreply.github.com>
2025-02-17 13:43:42 +0000

    Further HTML additions

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
 <html>
+    
+
+
 <head>
-<title>Page Title</title>
+<title> Connection made easier </title>
+<link rel = "stylesheet" href="log-in.css">
 </head>
+
 <body>
+    <h1 id = "bannerText">The Bugle</h1>
+    <div class = "logInForm">
+        <form action="log-in.php" method = "post">
+            <label for="username">Username:</label><br>
+            <input type="email" placeholder = "Enter Email" name="username"><br>
+
+            <label for="password">Password: :</label><br>
+            <input type="password" placeholder = "Enter Password"  name="password">
+        </form>
+
+        <br>
+
+        <button type = "submit">Login</button>
+
+        <label>
+                <input type="checkbox" checked="checked" name="remember">
+                Remember me  
+        </label>
+
+    
+
+        <br></br>
+
+        <label> <a href="signup.html">Sign-up here</a> </label>
+    </div>
 
     <?php
-        echo "Hello World";
+        
     ?>
 
 </body>
