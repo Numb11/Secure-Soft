6735d53f2a722f7a13112bb45b002c4e5c1ff5a5
Soup <87945079+Numb11@users.noreply.github.com>
2025-02-20 15:09:22 +0000

    Added simple log-in input validation
    Used RegEX to enforce password rules, need to sanitise for SQL

--- index.php
+++ index.php
@@ -1,13 +1,45 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
+    
 
-    <?php
-        echo "Hello World";
-    ?>
+
+<head>
+<title> FakeBook </title>
+<link rel = "stylesheet" href="log-in.css">
+<link rel="icon" href="favi.png">
+</head>
+
+<body>
+    <h1 id = "bannerText">Connection made easier </h1>
+    <div class = "logInForm">
+        <form action="verifyUser.php" method = "POST">
+            <label for="username">Username:</label><br>
+            <input type="text" placeholder = "Enter Username" name="username" required><br>
+
+            <label for="password">Password: :</label><br>
+            <input type="password" placeholder = "Enter Password"  name="password" required>
+
+        <br>
+
+        <button type = "submit">Login</button>
+
+        <label>
+                <input type="checkbox" checked="checked" name="remember">
+                Remember me  
+        </label>
+
+    
+
+        <br></br>
+
+        <label> <a href="signup.html">Sign-up here</a> </label>
+
+        <br></br>
+
+        </form>
+
+        
+    </div>
 
 </body>
 </html>
---
+++ verifyUser.php
@@ -0,0 +1,61 @@
+<?php
+
+
+    // Prevent Brute Force through attempt limit, could IP ban
+    if (!isset($attempts)) {
+        $attempts = 50; //change to adjust login attempt
+    } 
+    session_start();
+    if (!isset($_SESSION["logAtt"])) {
+        $_SESSION["logAtt"] = 0;
+    } 
+    elseif ($_SESSION["logAtt"] >= $attempts){
+        echo "Access Denied, too many attempts";
+        exit();
+    }else{
+        $_SESSION["logAtt"]++; //change for unlimited attempt
+    }
+
+    $remainingAtt = $attempts - $_SESSION["logAtt"]
+    function validateCred($username, $password)
+        {
+
+        $passPatt = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{14,50}$/"; //geeksForGeeks
+
+
+        if (strlen($username) < 2 || strlen($username) > 25)
+            {
+                echo "Error, username too" . (($username > 2) ? " short":" long") . " please retry";
+                exit("Attempts remaining: ". $attempts - $_SESSION["logAtt"]);
+            } 
+        elseif (strlen($password) < 12 || strlen($password) > 50)
+            {
+                echo "Error, password too" . (($username > 12) ? " short":" long") . " please retry";
+                exit("Attempts remaining: ". $attempts - $_SESSION["logAtt"]);
+            }
+        elseif (!(preg_match($passPatt,$password, $matches))){
+            echo "Password error, a password should contain the following <br> 1. Length between 14 and 50 <br> 2. At least one digit/special character <br> 3. At least one uppercase/lowercase character <br> ";
+            exit("Attempts remaining: " . $attempts - $_SESSION["logAtt"]);
+        }
+        }
+
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    validateCred($username, $password)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
