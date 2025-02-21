a508f13475e1e1dd4efac4da708a2a097ae11bb0
Soup <87945079+Numb11@users.noreply.github.com>
2025-02-21 15:38:36 +0000

    Added session timeout and fixed session logic

--- verifyUser.php
+++ verifyUser.php
@@ -2,21 +2,39 @@
 
     // Prevent Brute Force through attempt limit, could IP ban
     if (!isset($attempts)) {
-        $attempts = 50; //change to adjust login attempt
+        $attempts = 3; //change to adjust login attempt
     } 
     session_start();
-    if (!isset($_SESSION["logAtt"])) {
+
+    if (!isset($_SESSION["logAtt"])) {        
         $_SESSION["logAtt"] = 0;
-    } 
-    elseif ($_SESSION["logAtt"] >= $attempts){
+        $_SESSION["remainingAtt"] = ($attempts - $_SESSION["logAtt"]);
+        $_SESSION["login_time_stamp"] = time();
+    }else{
+        if ((time()- $_SESSION["login_time_stamp"]) > 50){ //Session should end after 50 seconds
+            echo "Session timeout, please refresh";
+            session_unset();
+            session_destroy();
+            exit();
+
+        }
+
+    }
+    if ($_SESSION["logAtt"] >= $attempts){
         echo "Access Denied, too many attempts";
         exit();
     }else{
         $_SESSION["logAtt"]++; //change for unlimited attempt
+        $_SESSION["remainingAtt"]--;
     }
 
-    $remainingAtt = $attempts - $_SESSION["logAtt"];
-    function validateCred($username, $password, $remainingAtt)
+
+    function endSess(){
+        exit("Attempts remaining: " . $_SESSION["remainingAtt"]);
+
+    }
+
+    function validateCred($username, $password)
         {
 
         $passPatt = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{14,50}$/"; //geeksForGeeks
@@ -25,16 +43,18 @@
         if (strlen($username) < 2 || strlen($username) > 25)
             {
                 echo "Error, username too" . (($username > 2) ? " short":" long") . " please retry";
-                exit("Attempts remaining: ". $remainingAtt);
+                endSess();
+
             } 
         elseif (strlen($password) < 12 || strlen($password) > 50)
             {
                 echo "Error, password too" . (($username > 12) ? " short":" long") . " please retry";
-                exit("Attempts remaining: ". $remainingAtt);
+                endSess();
             }
         elseif (!(preg_match($passPatt,$password, $matches))){
             echo "Password error, a password should contain the following <br> 1. Length between 14 and 50 <br> 2. At least one digit/special character <br> 3. At least one uppercase/lowercase character <br> ";
-            exit("Attempts remaining: " . $remainingAtt);
+            endSess();
+            
         }
         }
 
@@ -42,7 +62,9 @@
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    validateCred($username, $password, $remainingAtt);
+
+
+    validateCred($username, $password);
 
 ?>
 
