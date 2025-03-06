32792b68c91d28065b75ec9ef02a5797eea6585d
tomwalker137 <161866981+tomwalker137@users.noreply.github.com>
2025-03-06 15:25:27 +0000

    Update signUphtml.php

--- signUphtml.php
+++ signUphtml.php
@@ -15,19 +15,24 @@
     <h1 id = "bannerText"> Sign-up! </h1>
     <div class = "signUpFormArea">
         <form action="signUp.php" id="signUpForm" method = "POST">
-        <label for="email">Email: </label> <br>
-            <input type = "email" placeholder = "Enter email" name="email"><br>
+        <label for="email">Email: (verified emails only!) </label> <br>
+        <br>
+            <input type = "email" placeholder = "Enter email" name="email" required><br>
             <br>
 
             <label for="username">Username: </label> <br>
-            <input type = "username" placeholder = "Enter username" name="username"><br>
+            <br>
+            <input type = "username" placeholder = "Enter username" name="username" required><br>
             <br>
 
             <label for="password">password: </label> <br>
-            <input type = "password" placeholder = "Enter password" name="password"><br>
+            <br>
+            <input type = "password" placeholder = "Enter password" name="password" required><br>
 
+            <br>
 
-            <button type = "submit" id = "verButt">Log-in</button> 
+            <button type = "submit" name = "submit">confirm details</button> 
+            <br>
             <br>
             <label> <a href="index.php">Log-in here!</a> </label>
 
